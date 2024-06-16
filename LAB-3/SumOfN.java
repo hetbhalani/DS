517142fadbe3735da18c80ecf5e1fb72cb936982
Het Bhalani <bhalanihet2006@gmail.com>
2024-06-16 23:21:18 +0530

    lab-3

---
+++ LAB-3/SumOfN.java
@@ -0,0 +1,25 @@
+//3
+
+import java.util.Scanner;
+
+public class SumOfN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter length of arr");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        
+
+        for(int i =0; i< n; i++){
+            a[i]=sc.nextInt();
+        }
+        int sum = 0;
+
+        for(int i = 0; i<n; i++){
+            sum += a[i];
+        }
+
+        int avg = sum/n;
+        System.out.println(avg);
+    }
+}
