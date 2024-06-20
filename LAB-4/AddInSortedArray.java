5d819a50becb4fd28716274968851255e8686677
Het Bhalani <bhalanihet2006@gmail.com>
2024-06-20 16:05:06 +0530

    lab-4 added

---
+++ LAB-4/AddInSortedArray.java
@@ -0,0 +1,26 @@
+public class AddInSortedArray {
+    public static void main(String[] args) {
+        int element = 4;
+        int n1 [] = {1,1,2,3,5,7,9};
+        int n = 7;
+        int n2 [] = new int [n + 1];
+        
+      int i = 0;
+      while (n1[i] < element) {
+        n2[i] = n1[i];
+        i++;
+      }
+
+      n2[i] = element;
+      i++;
+
+      while (i < n+1) {
+        n2[i] = n1[i-1];
+        i++; 
+      }
+
+      for(int j = 0; j<n +1; j++){
+        System.out.print(n2[j] + " ");
+      }
+    }    
+}
---
+++ LAB-4/DeleteElementFromArray.java
@@ -0,0 +1,18 @@
+public class DeleteElementFromArray {
+    public static void main(String[] args) {
+        int location = 4;
+        int length = 5;
+
+        int n1 [] = {1,4,7,2,3};
+
+        for(int i=location; i<length-1; i++){
+            
+            n1[i] = n1[i+1];
+           
+        }
+
+        for(int i = 0; i<length-1; i++){
+            System.out.print(n1[i] + " ");
+           }
+    }    
+}
