ccb0ae284c706d2d25c60f2093b425c22f59365f
hetbhalani <bhalanihet2006@gmail.com>
2024-08-04 22:54:32 +0530

    remove duplicates

---
+++ LAB-12/RemoveDuplicates.java
@@ -0,0 +1,72 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class RemoveDuplicates{
+    private ListNode head;
+
+    public void addLast(int val){
+        ListNode newNode = new ListNode(val);
+        if(head == null){
+            head = newNode;
+        } 
+        else{
+            ListNode current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public ListNode removeDuplicates(){
+        if (head == null) {
+            return null;
+        }
+        
+        ListNode current = head;
+        
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val){
+                current.next = current.next.next;
+
+            }
+             else{
+                current = current.next;
+            }
+        }
+        
+        return head;
+    }
+
+    public void printList() {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicates list = new RemoveDuplicates();
+
+
+        list.addLast(1);
+        list.addLast(1);
+        list.addLast(6);
+        list.addLast(13);
+        list.addLast(13);
+        list.addLast(13);
+        list.addLast(27);
+        list.addLast(27);
+
+        list.printList();
+    }
+}
