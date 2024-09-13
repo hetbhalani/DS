d44883b050d9e2f5330b7dd65ce277df818d52fe
hetbhalani <bhalanihet2006@gmail.com>
2024-09-13 23:03:26 +0530

    travvvv treeee

--- Test/BinaryTreeDemo.java
+++ Test/BinaryTreeDemo.java
@@ -23,8 +23,19 @@ public class BinaryTreeDemo {
             newNode.right = buildTree(nodes);
 
             return newNode;
-        }   
+        }  
     }
+
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    
 
     public static void main(String[] args) {
         int nodes [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -33,5 +44,7 @@ public class BinaryTreeDemo {
        Node root = tree.buildTree(nodes);
 
        System.out.println(root.data);
+
+       preorder(root);
     }
 }
