fcb246a3478916d1490a572a84ec16b728be593b
hetbhalani <bhalanihet2006@gmail.com>
2024-09-14 00:28:49 +0530

    travvvv treeee

--- Test/BinaryTreeDemo.java
+++ Test/BinaryTreeDemo.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreeDemo {
     static class Node{
         int data;
@@ -39,20 +42,47 @@ public class BinaryTreeDemo {
         if(root == null){
             return;
         }
-        preorder(root.left);
+        inorder(root.left);
         System.out.print(root.data + " ");
-        preorder(root.right);
+        inorder(root.right);
     }
 
     public static void postorder(Node root){
         if(root == null){
             return;
         }
-        preorder(root.left);
-        preorder(root.right);
+        postorder(root.left);
+        postorder(root.right);
         System.out.print(root.data + " ");
     }
 
+    public static void levelorder(Node root){
+        if(root == null) return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node curr = q.remove();
+
+            if(curr == null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.println(curr.data);
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+                
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int nodes [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -62,10 +92,10 @@ public class BinaryTreeDemo {
 
        System.out.println(root.data);
 
-    //    preorder(root);
-    //    System.out.println();
+       preorder(root);
+       System.out.println();
        inorder(root);
-    //    System.out.println();
-    //    postorder(root);
+       System.out.println();
+       postorder(root);
     }
 }
