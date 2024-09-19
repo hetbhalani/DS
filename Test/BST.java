b190bffc0e8b939f5feddac1127fda1b66b16374
hetbhalani <bhalanihet2006@gmail.com>
2024-09-19 09:16:49 +0530

    print whole path

--- Test/BST.java
+++ Test/BST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BST {
     static class Node{
         int data;
@@ -100,6 +102,27 @@ public class BST {
         }
     }
 
+    public static void printRootToLeaf(Node root, ArrayList<Integer> path){
+        if(root == null)return;
+
+        path.add(root.data);
+
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+        else{
+            printRootToLeaf(root.left, path);
+            printRootToLeaf(root.right, path);
+            path.remove(path.size() -1);
+        }
+    }
+
+    public static void printPath(ArrayList<Integer> path){
+        for(int i = 0; i<path.size();i++){
+            System.out.print(path.get(i) + " -> ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int values [] = {5,1,3,4,2,7};
         Node root = null;
@@ -118,5 +141,8 @@ public class BST {
         System.out.println();
 
         printInRange(root, 2, 10);
+        System.out.println();
+
+        printRootToLeaf(root, new ArrayList<>());
     }
 }
